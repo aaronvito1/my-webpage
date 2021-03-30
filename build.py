7e8ccd7e489c37a1d8eeb8ab90e7004665788f5b
Aaron V <aaronvito97@gmail.com>
2021-03-30 12:14:59 -0700

    changed casing of file names

--- build.py
+++ build.py
@@ -6,56 +6,56 @@ bottom_html_file = open('./templates/bottom.html').read()
 
 # Use contents dir to add the middle parts to complete the code
 # Currently Home 
-middle_html_file = open('./content/Home.html').read()
+middle_html_file = open('./content/index.html').read()
 
 print('HTML files combined')
 
 combined_html_files = top_html_file + middle_html_file + bottom_html_file
 print(combined_html_files)
 
-open('Home.html', 'w+').write(combined_html_files)
+open('index.html', 'w+').write(combined_html_files)
 
 
 # About 
 # Use contents dir to add the middle parts to complete the code
-middle_html_file = open('./content/About.html').read()
+middle_html_file = open('./content/about.html').read()
 
 print('HTML files combined')
 
 combined_html_files = top_html_file + middle_html_file + bottom_html_file
 print(combined_html_files)
 
-open('About.html', 'w+').write(combined_html_files)
+open('about.html', 'w+').write(combined_html_files)
 
 # Portfolio
 # Use contents dir to add the middle parts to complete the code
-middle_html_file = open('./content/Portfolio.html').read()
+middle_html_file = open('./content/portfolio.html').read()
 
 print('HTML files combined')
 
 combined_html_files = top_html_file + middle_html_file + bottom_html_file
 print(combined_html_files)
 
-open('Portfolio.html', 'w+').write(combined_html_files)
+open('portfolio.html', 'w+').write(combined_html_files)
 
 # Blog
 # Use contents dir to add the middle parts to complete the code
-middle_html_file = open('./content/Blog.html').read()
+middle_html_file = open('./content/blog.html').read()
 
 print('HTML files combined')
 
 combined_html_files = top_html_file + middle_html_file + bottom_html_file
 print(combined_html_files)
 
-open('Blog.html', 'w+').write(combined_html_files)
+open('blog.html', 'w+').write(combined_html_files)
 
 # Contact
 # Use contents dir to add the middle parts to complete the code
-middle_html_file = open('./content/Contact.html').read()
+middle_html_file = open('./content/contact.html').read()
 
 print('HTML files combined')
 
 combined_html_files = top_html_file + middle_html_file + bottom_html_file
 print(combined_html_files)
 
-open('Contact.html', 'w+').write(combined_html_files)
+open('contact.html', 'w+').write(combined_html_files)
