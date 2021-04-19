9a714e4dec864b234f78ea3f2012ec8182bb5556
Aaron V <aaronvito97@gmail.com>
2021-04-19 15:07:28 -0700

    added more content to extra section

--- manage.py
+++ manage.py
@@ -1,18 +1,18 @@
 import util
-import sys
-print("This is argv:", sys.argv)
-print("Number of arguements: ", len(sys.argv))
-print ("The arguments are: " , str(sys.argv))
-command = sys.argv[1]
-print(command)
-if command == "build":
-    print("Build was specified")
-# TODO: Do something here... elif command == "new":
-elif command == "new":
-    print("New page was specified")
-# TODO: Do something here... else:
-else:
-    print("Please specify ’build’ or ’new’")
+# import sys
+# print("This is argv:", sys.argv)
+# print("Number of arguements: ", len(sys.argv))
+# print ("The arguments are: " , str(sys.argv))
+# command = sys.argv[1]
+# print(command)
+# if command == "build":
+#     print("Build was specified")
+# # TODO: Do something here... elif command == "new":
+# elif command == "new":
+#     print("New page was specified")
+# # TODO: Do something here... else:
+# else:
+#     print("Please specify ’build’ or ’new’")
 
 
 util.main()
