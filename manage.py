074acfa7bb5edb452ce4a29b63ae9a477147df34
Aaron V <aaronvito97@gmail.com>
2021-04-22 20:36:32 -0700

    cleaned html and python

--- manage.py
+++ manage.py
@@ -1,21 +1,20 @@
 import util
-# import sys
-# print("This is argv:", sys.argv)
-# print("Number of arguements: ", len(sys.argv))
-# print ("The arguments are: " , str(sys.argv))
-# command = sys.argv[1]
-# print(command)
-# if command == "build":
-#     print("Build was specified")
-# # TODO: Do something here... elif command == "new":
+import sys
+
+print("This is argv:", sys.argv)
+print("Number of arguements: ", len(sys.argv))
+print ("The arguments are: " , str(sys.argv))
+command = sys.argv[1]
+
+print(command)
+if command == "build":
+	print("Build was specified")
+
+	util.build_pages()
+	print("Import successful")
+# TODO: Do something here... elif command == "new":
 # elif command == "new":
 #     print("New page was specified")
 # # TODO: Do something here... else:
-# else:
-#     print("Please specify ’build’ or ’new’")
-
-
-util.main()
-print("Import successful")
-
-
+else:
+    print("Please specify ’build’ or ’new’")
--- util.py
+++ util.py
@@ -2,64 +2,61 @@ from jinja2 import Template
 import glob 
 import os
 
-# list of files that only contains html extensions 
-all_content_html_files = sorted(glob.glob("./content/*.html"))
 
-# print(all_content_html_files)
-pages = [] 
-
-def generate_list(list):
+def generate_list(pages, list):
 	for file in list :
-		if file != "./content/index.html" :
-			file_path = file
-			# print(file_path)÷
-			file_name = os.path.basename(file_path)
-			# print(file_name)
-			name_only, extension = os.path.splitext(file_name)
-			# print(name_only)
+		file_path = file
+		# print(file_path)÷
+		file_name = os.path.basename(file_path)
+		# print(file_name)
+		name_only, extension = os.path.splitext(file_name)
+		# print(name_only)
 
-			new_list = pages.append({
-				"filename": file_path,
-				"title": name_only,
-				"output": 'docs/'+file_name,
-				"output_filename": './'+file_name,
-			})
+		new_list = pages.append({
+			"filename": file_path,
+			"title": name_only,
+			"output": 'docs/'+file_name,
+			"output_filename": './'+file_name,
+		})
 	return new_list
 
-generate_list(all_content_html_files)
 
-def main():
+def apply_template(pages, page, content):
+	content_html = open(content).read()
+	# read in combined top/bottom template 
+	# print(content_html)
+	template_html = open("./templates/base.html").read()
+	template = Template(template_html)
+	finished_page =  template.render(
+		pages= pages,
+		title= page["title"],
+		output= page["output"],
+		content= content_html,
+		output_filename= page["output_filename"]
+		)
+	open(page["output"], "w+").write(finished_page)
+	results = page["output"]
+	# # print(results)
+	return results
+
+def build_pages():
+
+	# list of files that only contains html extensions 
+	all_content_html_files = sorted(glob.glob("./content/*.html"))
+
+	# print(all_content_html_files)
+	pages = [] 
+
+	generate_list(pages, all_content_html_files)
+
 	for page in pages:
 		# def read_in_file(filename):
 		# 	filename_content = open(filename).read()
 		# 	print(filename_content) 
 		# 	return filename_content
-		def apply_template(content):
-			content_html = open(content).read()
-			# read in combined top/bottom template 
-			# print(content_html)
-			template_html = open("./templates/base.html").read()
-			template = Template(template_html)
-			finished_page =  template.render(
-				pages= pages,
-				title= page["title"],
-				output= page["output"],
-				content= content_html,
-				output_filename= page["output_filename"]
-				)
-			open(page["output"], "w+").write(finished_page)
-			results = page["output"]
-			# # print(results)
-			return results
 
-		# print('------------------------------')
-		# print(page["title"])
-		# print(page["filename"])
-		# print(page["output"])
 		content = page["filename"]
 		# print(content)
-		resulting_html_for_doc = apply_template(content)
+		resulting_html_for_doc = apply_template(pages, page, content)
 
-main()
 
-# print(pages)
